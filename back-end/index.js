9eb4c0c8ddca04f2663537c70919a867cb7859d9
Alexander Santos <alexsantosantana@live.com>
2019-10-23 22:40:25 -0400

    Adding routes and organizing code

--- back-end/index.js
+++ back-end/index.js
@@ -2,13 +2,11 @@ const express = require('express');
 const session = require('express-session');
 const crypto = require('crypto');
 const io = require('socketio');
-const times = require('lodash/times');
-const random = require('lodash/random');
 const bodyParser = require('body-parser');
-const faker = require('faker');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const jsonParser = bodyParser.json();
 const db = require('./models');
+const createData = require('./fakerData');
 const app = express();
 
 const myStore = new SequelizeStore({
@@ -28,30 +26,12 @@ myStore.sync();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res, next) => {
+	res.send('Hello World');
+});
+
 app.get('/createUsers', (req, res, next) => {
-	db.user
-		.bulkCreate(
-			times(10, () => ({
-				email: `${faker.name.firstName()}@${faker.name.lastName()}.com`,
-				password: '123456'
-			}))
-		)
-		.then(console.log);
-	db.room
-		.bulkCreate(
-			times(10, () => ({
-				hostId: random(1, 10),
-				roomName: faker.name.jobArea()
-			}))
-		)
-		.then(console.log);
-	db.message.bulkCreate(
-		times(10, () => ({
-			userId: random(1, 10),
-			roomId: random(1, 10),
-			message: faker.lorem.sentence(20)
-		}))
-	);
+	createData(db);
 });
 
 app.listen(3001, () => {
@@ -59,8 +39,7 @@ app.listen(3001, () => {
 });
 
 //! Apollo set up
-const { ApolloServer, gql } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 const apolloServ = new ApolloServer({
