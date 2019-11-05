81640df17a38fc4e44c49a9e7205d858e9bbb4cc
Alexander Santos <alexsantosantana@live.com>
2019-11-05 04:15:01 -0500

    Azure attempt

---
+++ front-end/build/precache-manifest.0fccba6c1149f681ede506eaa76055b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5511370a93fea1e27d6c9bb955a9c77b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c80d2a54bba72b797ec",
+    "url": "/static/css/main.057f62a3.chunk.css"
+  },
+  {
+    "revision": "c95f2c38d552eda657f9",
+    "url": "/static/js/2.617f62fa.chunk.js"
+  },
+  {
+    "revision": "8c80d2a54bba72b797ec",
+    "url": "/static/js/main.a54921cb.chunk.js"
+  },
+  {
+    "revision": "eb1e8cf89a7826bb5e8b",
+    "url": "/static/js/runtime-main.0ac378fe.js"
+  }
+]);
