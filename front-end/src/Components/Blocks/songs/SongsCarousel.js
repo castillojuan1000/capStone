3c4946329347d04a49272f2726552050322f1151
Alexander Santos <alexandersantos@Alexanders-MacBook-Pro.local>
2019-11-15 13:58:52 -0500

    Optimizing the room view

--- front-end/src/Components/Blocks/songs/SongsCarousel.js
+++ front-end/src/Components/Blocks/songs/SongsCarousel.js
@@ -18,45 +18,31 @@ export default function SongCarousel({
     activeSlide: 0,
     activeSlide2: 0
   });
-  const settings = {
-    className: "likes-container",
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    beforeChange: (current, next) => {
-      setSlide({ activeSlide: next });
-      setRoomState({ ...roomState, currentSong: { ...roomState.songs[next] } });
-    },
-    afterChange: current => setSlide({ activeSlide2: current })
+  const ArtworkJSX = () => {
+    return roomState.songs.map((songObj, i) => {
+      const currentSong = songObj;
+      return (
+        <div className="room-player" key={i}>
+          <div className="room-art">
+            {currentSong && (
+              <img
+                className="room-art-image"
+                src={currentSong.image}
+                alt={`${currentSong.name} artwork`}
+              />
+            )}
+          </div>
+          <div className="room-currentsong-info">
+            <h3>{currentSong.artists[0].name}</h3>
+            <h3>{currentSong.name}</h3>
+          </div>
+        </div>
+      );
+    });
   };
   return (
     <>
-      <Slider {...settings}>
-        {roomState.songs.map((songObj, i) => {
-          const currentSong = songObj;
-          return (
-            <div key={i}>
-              <div className="room-player">
-                <div className="room-art">
-                  {currentSong && (
-                    <img
-                      className="room-art-image"
-                      src={currentSong.image}
-                      alt={`${currentSong.name} artwork`}
-                    />
-                  )}
-                </div>
-                <div className="room-currentsong-info">
-                  <h3>{currentSong.artists[0].name}</h3>
-                  <h3>{currentSong.name}</h3>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </Slider>
+      <ArtworkJSX />
     </>
   );
 }
